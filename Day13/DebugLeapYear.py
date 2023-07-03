bc8259f915636a0f1813406388086d6b620cdaf5
AkshayPrasad25 <akshayprasad510@gmail.com>
2023-07-03 22:23:14 +0530

    Day 13

---
+++ Day13/DebugLeapYear.py
@@ -0,0 +1,29 @@
+#Learning debugging in python
+# Before debug
+# year=input("Which year do you want to check?")
+
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print("Leap year")
+#         else:
+#             print("Not leap year")
+#     else:
+#         print("Leap year")
+# else:
+#     print("Not leap year")
+
+
+# After debug
+year=int(input("Which year do you want to check?"))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year")
+        else:
+            print("Not leap year")
+    else:
+        print("Leap year")
+else:
+    print("Not leap year")
---
+++ Day13/DebugOddEven.py
@@ -0,0 +1,15 @@
+#Learning debugging in python
+# Before debug
+# number=int(input("Which number do you want to check?"))
+# if number % 2 = 0:
+#     print("This is an even number")
+# else:
+#     print("This is an odd number")
+
+
+# After debug
+number=int(input("Which number do you want to check?"))
+if number % 2 == 0:
+    print("This is an even number")
+else:
+    print("This is an odd number")
